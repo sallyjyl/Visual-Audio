31d803a5835dfe49ede6ef5ea4a976a41e897952
henryxu1997 <henryxu1997@users.noreply.github.com>
2018-09-16 01:40:12 -0400

    Add files via upload
    Updated file, combined content analysis with image search

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -3,10 +3,27 @@ from google.cloud import language_v1
 from google.cloud.language_v1 import enums
 import six
 
+from google.cloud import language
 
-def analyze_sentiment(content):
+import argparse
+import sys
+
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+import six
+
+import requests
+
+#input: list of strings
+#output: list of sentiment scores matched by index
+
+
+def sample_analyze_sentiment(content):
 
     client = language_v1.LanguageServiceClient()
+
+    # content = 'Your text to analyze, e.g. Hello, world!'
 
     if isinstance(content, six.binary_type):
         content = content.decode('utf-8')
@@ -16,6 +33,53 @@ def analyze_sentiment(content):
 
     response = client.analyze_sentiment(document)
     sentiment = response.document_sentiment
-
+    print('Score: {}'.format(sentiment.score))
+    print('Magnitude: {}'.format(sentiment.magnitude))
     return sentiment.score
+
+
+
+def classify_text(text):
+    """Classifies content categories of the provided text."""
+    client = language.LanguageServiceClient()
+
+    if isinstance(text, six.binary_type):
+        text = text.decode('utf-8')
+
+    document = types.Document(
+        content=text.encode('utf-8'),
+        type=enums.Document.Type.PLAIN_TEXT)
+
+    categories = client.classify_text(document).categories
+
+    for category in categories:
+        print(u'=' * 20)
+        print(u'{:<16}: {}'.format('name', category.name))
+        print(u'{:<16}: {}'.format('confidence', category.confidence))
+        return category.name.split(' ')[-1] #only the last, most specific noun as the image to be displayed
+        # return category.name
+
+# print(classify_text("hello, i am very very happy lol true mountain hahah hahahah mountain hahah hahah mountain mountain mountain mountain mountain"))
+
+test_text = "hello, i am very very happy lol true mountain hahah hahahah mountain hahah hahah mountain mountain mountain mountain mountain"
+
+def search_for_image(input_keyword):
+    search_query = "https://www.googleapis.com/customsearch/v1?key=AIzaSyCVPDYrTm2h_keElXO1iAW-PW5RAlujEtg&cx=017480567514037437480%3Akvmd1lv2ahm&q=" + str(input_keyword) + "&searchType=image&fileType=jpg&imgSize=medium&num=1"
+    # r = requests.get("https://www.googleapis.com/customsearch/v1?key=AIzaSyCVPDYrTm2h_keElXO1iAW-PW5RAlujEtg&cx=017480567514037437480%3Akvmd1lv2ahm&q=flower&searchType=image&fileType=jpg&imgSize=medium&num=1")
+    r = requests.get(search_query)
+    # print(r.status_code)
+    # print(r.json())
+
+    intermediate = r.json()
+
+
+    return intermediate["items"][0]["link"]
+    # print(intermediate["items"][0]["link"])
+
+# "https://www.googleapis.com/customsearch/v1?key=AIzaSyCVPDYrTm2h_keElXO1iAW-PW5RAlujEtg&cx=017480567514037437480%3Akvmd1lv2ahm&q=flower&searchType=image&fileType=jpg&imgSize=medium"
+# print(search_for_image("flower"))
+
+# print(classify_text(test_text))
+print(search_for_image(str(classify_text(test_text))))
+
 
